eccba1cba187b952613bd86da2036ca553d5d2e2
Kevin Luo <kvn.luo@gmail.com>
2020-11-20 10:03:41 -0500

    navbar styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '20px 10px',
+    background: '#24252A',
+  },
+  navList: {
+    listStyle: 'none',
+    '& > li': {
+      display: 'inline-block',
+      padding: '0 20px',
+      '& > a': {
+        transition: 'all 0.3s ease 0s',
+        '&:hover': {
+          color: '#0088a9'
+        }
+      }
+    }
+  },
+  rightButton: {
+    padding: '9px 25px',
+    backgroundColor: 'rgba(0,136,169,1)',
+    border: 'none',
+    borderRadius: '50px',
+    cursor: 'pointer',
+    transition: 'all 0.3s ease 0s',
+    '&:hover': {
+      backgroundColor: 'rgba(0,136,169,0.8)'
+    }
+  }
+}))
 
 export default function Nav() {
+  const classes = useStyles();
   return (
-    <div>
-      
+    <div className={classes.root}>
+      <nav>
+        <ul className={classes.navList}>
+          <li><a href="#" >Google Books</a></li>
+          <li><a href="#" >Search</a></li>
+          <li><a href="#" >Saved</a></li>
+        </ul>
+      </nav>
+      <a href="#"><button className={classes.rightButton} >Hello</button></a>
     </div>
   )
 }
