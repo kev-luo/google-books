0e3b2be04a66489bb420807400116d815a325673
Kevin Luo <kvn.luo@gmail.com>
2020-11-19 21:15:04 -0500

    connected to db

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use("/api", require("./routes/api"));
+app.use("/", require("./routes/root"));
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("connected to db");
+    app.listen(PORT, () => {
+      console.log(`API server listening on port ${PORT}`);
+    });
+  });
