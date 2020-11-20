43f9d3f57691fc2bb1bf87d8a0840f6fa9c4d8a4
Kevin Luo <kvn.luo@gmail.com>
2020-11-19 21:07:19 -0500

    rough outline of api and google search routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/books', async (req, res) => {
+  res.sendStatus(200);
+})
+
+router.post('/books', async (req, res) => {
+  res.sendStatus(200);
+})
+
+router.get('/books/:id', async (req, res) => {
   res.sendStatus(200);
 })
 
--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendStatus(200);
+router.get('/google', async (req, res) => {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${req.query.title}`
+  const books = await axios.get(url);
+  res.json(books);
 })
 
 module.exports = router;
